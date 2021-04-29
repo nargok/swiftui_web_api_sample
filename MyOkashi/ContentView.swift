dfea84a0baa6f4c430f8b0f9fdcde7cb20157d40
nargok <nargok@gmail.com>
2021-04-29 15:28:06 +0900

    Initial Commit

---
+++ MyOkashi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyOkashi
+//
+//  Created by 後閑諒一 on 2021/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by 後閑諒一 on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
